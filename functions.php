6292c4f97fd62f7da8e78392633193d6402a5939
daniellaskinner <daniellaskinner120@gmail.com>
2020-01-28 16:58:46 +0000

    created a form that only takes a specific password

--- functions.php
+++ functions.php
@@ -1,1 +1,20 @@
 <?php
+
+//function to check if credentials are correct and re-direct to about page if so
+function validateCredentials (string $usernameFromUser, string $passwordFromUser, string $username1, string $password1) {
+    if($usernameFromUser == $username1 && $passwordFromUser == $password1) {
+        header('Location: account.php');
+    } else {
+        return 'Invalid credentials, please try again';
+    }
+}
+
+
+
+//passsword hashing
+//function cleanseInput(string $userInput) :string {
+//    $cleansedInput = trim($userInput);
+//    $cleansedInput = stripslashes($userInput);
+//    $cleansedInput = htmlspecialchars($userInput);
+//    return $cleansedInput;
+//}
--- index.php
+++ index.php
@@ -2,10 +2,52 @@
 require_once 'functions.php';
 session_start();
 
-if(isset($_GET['username']) && isset($_GET['password'])) {
-    $username = $_GET['username'];
-    $password = $_GET['password'];
+//checking user enters some credentials and assigning these credentials to variables
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $username1 = 'daniellaskinner120@gmail.com';
+    $password1 = 'password123';
+
+    echo validateCredentials($username, $password, $username1, $password1);
 };
+
+
+
+
+//if user tries to navigate to the log in page when already logged in re-direct to account page
+
+
+//set the session variables for username and password
+//$_SESSION['username']=$username;
+//$_SESSION['password']=$password;
+//
+//
+//var_dump($_SESSION);
+
+
+
+
+
+//encrypting password
+
+//function encryptPass(string $password) :string {
+//$cleansedPass=cleanseInput($password);
+//$hashedPass = password_hash($cleansedPass, PASSWORD_DEFAULT);
+//$enterPassAgain = $password;
+//$isCorrect = password_verify ($enterPassAgain, $hashedPass);
+//return $isCorrect;
+//}
+
+//encryptPass($_GET['password']);
+//
+//if($isCorrect){
+//    echo "Password entered is correct";
+//} else {
+//    echo "Password entered is incorrect.";
+//}
+
+
 
 ?>
 
@@ -17,8 +59,8 @@ if(isset($_GET['username']) && isset($_GET['password'])) {
 
 <body>
     <h1>Please enter your log in details</h1>
-    <form action="account.php" method="get">
-        <input type="text" name="username" >
+    <form method="post">
+        <input type="email" name="username" >
         <input type="password" name="password">
         <input type="submit" name="submit">
     </form>
